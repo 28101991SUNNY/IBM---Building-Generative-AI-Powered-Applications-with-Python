477fe7ba53fbcf5d24b04350ed7303eea9a27089
Sunny Panchal <sunnypanchalin@gmail.com>
2024-07-13 10:39:12 -0400

    commit 2

--- automate_url_captioner_blip2.py
+++ automate_url_captioner_blip2.py
@@ -9,11 +9,11 @@ processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
 model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b")
 
 # Specify the directory where your images are
-image_dir = "/path/to/your/images"
+image_dir = "/Users/sunnypanchal/Desktop/Neovarsity DSML research papers/SCALER/IBM - Building Generative AI-Powered Applications with Python/images_for_captioning"
 image_exts = ["jpg", "jpeg", "png"]  # specify the image file extensions to search for
 
 # Open a file to write the captions
-with open("captions.txt", "w") as caption_file:
+with open("captions_from_folder.txt", "w") as caption_file:
     # Iterate over each image file in the directory
     for image_ext in image_exts:
         for img_path in glob.glob(os.path.join(image_dir, f"*.{image_ext}")):
